7df04fd75684a5b62e2d865bffb80029cb7d2127
dongjie <dongjie@lionbridgecapital.cn>
2018-04-02 10:35:05 +0800

    2

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -30,8 +30,8 @@ def populate():
 
     for cat, cat_data in cats.items():
         c = add_cat(cat)
-    for p in cat_data["pages"]:
-        add_page(c, p["title"], p["url"],p["views"])
+        for p in cat_data["pages"]:
+            add_page(c, p["title"], p["url"],p["views"])
     # Print out the categories we have added.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
